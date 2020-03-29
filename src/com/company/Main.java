57e7085056c566c22d6306a6d0c6df7e6324a4c6
Mason Gil <masonegil@gmail.com>
2020-03-29 18:51:14 -0500

    small update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 import java.io.*;
+import java.util.Vector;
 
 public class Main {
 
-    private static
+    private static Vector<Byte> program = new Vector<Byte>();
+    private void ReadFile (String file) {
+
+    }
     public static void main(String[] args) {
 	// write your code here
     }
