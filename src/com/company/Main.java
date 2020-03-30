42bb93b83f01935188a1f96315e514214ad4bbdf
Mason Gil <masonegil@gmail.com>
2020-03-29 19:40:05 -0500

    got it to read the binary file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,14 +16,16 @@ public class Main {
     private static void ReadFile (String file) {
         InputStream inputStream;
         byte[] current = new byte[4];
-        int i;
         try {
             inputStream = Files.newInputStream(Paths.get(file), StandardOpenOption.READ);
+            int i;
             while ((i = inputStream.read(current, 0, 4)) != -1) {
                 String byteString = "";
                 for (byte b: current) {
-                    byteString +=
+                    byteString += String.format("%8s", Integer.toBinaryString(b & 0xFF).replace(' ', '0'));
+//                    byteString += String.format("%8s", Integer.toBinaryString(b));
                 }
+                print(byteString.replace(' ', '0'));
             }
         } catch (IOException e) {
             e.printStackTrace();
